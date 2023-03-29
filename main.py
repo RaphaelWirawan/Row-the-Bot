15cc03e371e1a4d20fd08bacfc9b644641710e5c
RaphaelWirawan <alessandrowirawan@gmail.com>
2023-03-29 05:10:42 +0000

    webserver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from webserver import webserver
 #my_secret = os.environ['TOKEN']
 
 intents = discord.Intents.default()
@@ -7,16 +8,25 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+  print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+  if message.content.startswith('$orewaochinchingadaisukinandayo'):
+    await message.channel.send(file=discord.File('./memes/bidenblast.jpg'))
 
+  if message.content.startswith('$ass'):
+    await message.channel.send(file=discord.File('./memes/as.jpg'))
+
+  if message.content.startswith('$helpme'):
+    await message.channel.send(file=discord.File('./memes/indonesian.jpeg'))
+
+webserver()
 client.run(os.getenv('TOKEN'))
